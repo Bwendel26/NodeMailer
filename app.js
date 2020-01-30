9b4bcc5dcfadbd8f274a4e948a5dc229ef4bf881
Bruno Fernandes <bwendel26999@gmail.com>
2020-01-30 19:59:48 -0300

    Foi feita a codificacao necessaria para o envio de emails, faltou colocar os dados de conexao e configurar o umbler como hospedagem.

--- app.js
+++ app.js
@@ -5,7 +5,32 @@ const app = express()
 
 const port = 3000
 
+const user = "wallksignarf@gmail.com"
+const pass = "123159onurb123357"
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/send', (req, res) => {
+  
+   const transporter = nodeMailer.createTransport({
+      host:"smtp.umbler.com",
+      port: "", //Deveria entrar com os dados da umbler...
+      auth: { user, pass }
+   })
+
+   transporter.sendMail({
+      from: user,
+      to: "bwendel26999@gmail.com",
+      replyTo: user,
+      subject: "Seja bem vindo!",
+      text: "Ola, seja bem vindo a meu mailer"
+   }).then(info => {
+      res.send(info)
+   }).catch(error => {
+      res.send(error)
+   })
+
+})
 
 app.listen(port, () => console.log(`Running on port ${port}!`))
 
